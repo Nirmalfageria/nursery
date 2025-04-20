cc57fd0e260d67c4fe08ad0e4bbc5b502150faa0
nirmal fageria <nirmalfageria15@gmail.com>
2025-04-20 22:20:56 +0530

    Add database connection utility using Mongoose

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+const DB_NAME = nursery
+
+const DbConnect = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      
+    });
+
+    console.log(`\n Database connected successfully`);
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+    process.exit(1);
+  }
+};
+
+export default DbConnect;
