46e060d1855f644d49b8aee5ae624b7f56b7aa69
nirmal fageria <nirmalfageria15@gmail.com>
2025-05-09 09:39:58 +0530

    feat: Add plant model schema for MongoDB integration

---
+++ src/models/plants.model.js
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from "mongoose";
+
+const plantSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String, // URL to the plant image
+    required: true
+  },
+  description: {
+    type: String, // Care tips, sunlight, watering
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  category: {
+    type: String,
+    enum: ["Flowering", "Fruit", "Seasonal", "Indoor", "Outdoor"],
+    required: true
+  },
+  stock: {
+    type: Number,
+    required: true // Available quantity
+  }
+}, { timestamps: true });
+
+export default mongoose.models.Plant || mongoose.model("Plant", plantSchema);
